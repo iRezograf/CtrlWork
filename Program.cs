3ec0a4d816b2a09ea6c3d6fa6e5739ee32555e19
Rafael Rezyapov <rezograf@gmail.com>
2022-10-13 13:26:37 +0300

    add Program.cs. It works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
 ﻿Arrays a = new Arrays();
 string [] array = {"Hello","2","World",":-)"};
+//string[] array = { "RUSSIA", "DENMARK", "KAZAN" };
 
 a.PrintArray(array);
+
+int count = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+
+    if (array[i].Length <= 3) count++;
+
+string[] newArray = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+        newArray[j++] = array[i];
+
+Console.Write("-> ");
+
+if (newArray.GetLength(0) > 0)
+    a.PrintArray(newArray);
+else
+    Console.Write("[]");
